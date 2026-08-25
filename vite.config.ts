7d87c08a48a957c6ad27a30178930caba9680714
tepjey <athif194@yahoo.com>
2026-08-25 10:45:00 +0800

    fix: use standard @vitejs/plugin-react

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: "::",
